3701ed75de1239b15553d5ae2d0c12caf5f58da9
Kathleen Forgiarini da Silva <82175930+kathleenforgiarini@users.noreply.github.com>
2023-03-21 20:00:19 -0400

    Add files via upload

---
+++ lab7.1/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace lab7._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.fName = new System.Windows.Forms.TextBox();
+            this.lName = new System.Windows.Forms.TextBox();
+            this.write = new System.Windows.Forms.Button();
+            this.read = new System.Windows.Forms.Button();
+            this.exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "FName: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "LName: ";
+            // 
+            // fName
+            // 
+            this.fName.Location = new System.Drawing.Point(94, 29);
+            this.fName.Name = "fName";
+            this.fName.Size = new System.Drawing.Size(164, 22);
+            this.fName.TabIndex = 2;
+            // 
+            // lName
+            // 
+            this.lName.Location = new System.Drawing.Point(94, 60);
+            this.lName.Name = "lName";
+            this.lName.Size = new System.Drawing.Size(164, 22);
+            this.lName.TabIndex = 3;
+            // 
+            // write
+            // 
+            this.write.Location = new System.Drawing.Point(94, 119);
+            this.write.Name = "write";
+            this.write.Size = new System.Drawing.Size(93, 46);
+            this.write.TabIndex = 4;
+            this.write.Text = "Write text \r\nfile";
+            this.write.UseVisualStyleBackColor = true;
+            this.write.Click += new System.EventHandler(this.write_Click);
+            // 
+            // read
+            // 
+            this.read.Location = new System.Drawing.Point(254, 119);
+            this.read.Name = "read";
+            this.read.Size = new System.Drawing.Size(93, 46);
+            this.read.TabIndex = 5;
+            this.read.Text = "Read text \r\nfile";
+            this.read.UseVisualStyleBackColor = true;
+            this.read.Click += new System.EventHandler(this.read_Click);
+            // 
+            // exit
+            // 
+            this.exit.Location = new System.Drawing.Point(370, 181);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(93, 26);
+            this.exit.TabIndex = 6;
+            this.exit.Text = "Exit";
+            this.exit.UseVisualStyleBackColor = true;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 233);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.read);
+            this.Controls.Add(this.write);
+            this.Controls.Add(this.lName);
+            this.Controls.Add(this.fName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox fName;
+        private System.Windows.Forms.TextBox lName;
+        private System.Windows.Forms.Button write;
+        private System.Windows.Forms.Button read;
+        private System.Windows.Forms.Button exit;
+    }
+}
+
---
+++ lab7.1/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+// CODED BY KATHLEEN FORGIARINI DA SILVA
+// 2023-03-21
+// LAB 7.1 - WORKING WITH TEXT FILES
+
+namespace lab7._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        string path = @".\files\Names.txt";
+        private void write_Click(object sender, EventArgs e)
+        {
+            string dir = @".\files\";
+            if (!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
+            try
+            {
+                FileStream fileStream = new FileStream(path, FileMode.Append, FileAccess.Write);
+                StreamWriter writer = new StreamWriter(fileStream);
+
+                writer.Write(fName.Text + ";" + lName.Text + ";" + DateTime.Now.ToString() + "\n");
+
+                writer.Close();
+                fileStream.Close();
+
+                MessageBox.Show("Data was written to the file successfully.");
+            
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occured, try again. \n" + ex.Message);
+            }
+        }
+
+
+        private void read_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
+                StreamReader reader = new StreamReader(fileStream);
+
+                string textToPrint = "";
+                int counter = 0;
+                while (reader.Peek() != -1)
+                {
+                    string line = reader.ReadLine();
+                    string[] data = line.Split(';');
+                    textToPrint += data[0] + " " + data[1] + "\n";
+                    counter++;
+
+                    if (counter == 3)
+                    {
+                        MessageBox.Show(textToPrint);
+                        textToPrint = "";
+                        counter = 0;
+                    }
+                }
+
+                if (counter != 0)
+                {
+                    MessageBox.Show(textToPrint);
+                }
+
+                reader.Close();
+                fileStream.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred while reading from the file: " + ex.Message);
+            }
+        }
+    }
+}
